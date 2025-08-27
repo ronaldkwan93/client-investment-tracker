de0074ed2b0852000ee703609d80cf822e53084d
ronaldkwan93 <ronaldkwan93@gmail.com>
2025-08-27 13:43:14 +1000

    adds navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 
+import NavBar from "./components/NavBar/NavBar";
 import "./index.css";
 
 function App() {
   return (
     <>
-      <div className="flex justify-center flex-col items-center  ">
-        <h1 className="text-3xl font-bold underline">Hello</h1>
-        <h2>hi</h2>
-      </div>
+      <NavBar/>
     </>
   );
 }
