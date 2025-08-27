d3a2df03baeeedc2ff8769cc0306399f8f831f61
ronaldkwan93 <ronaldkwan93@gmail.com>
2025-08-27 21:48:22 +1000

    added data-service and contextapi

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import NavBar from "./components/NavBar/NavBar";
 import "./index.css";
 import Dashboard from "./pages/Dashboard";
 import Investments from "./pages/Investments";
+import PropertyContextProvider from "./context/PropertyContextProvider";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/investments" element={<Investments/>}/>
-        </Routes>
-      </BrowserRouter>
+      <PropertyContextProvider>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/investments" element={<Investments />} />
+          </Routes>
+        </BrowserRouter>
+      </PropertyContextProvider>
     </>
   );
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,15 +1,33 @@
+import { useContext } from "react";
+import { PropertyContext } from "../context/PropertyContextProvider";
+
 const Dashboard = () => {
+  const context = useContext(PropertyContext);
+  if (!context)
+    throw new Error("PropertyList must be used within PropertyContextProvider");
+
+  const { properties } = context;
+
   return (
     <div className="m-1 h-screen flex flex-col items-center gap-5 pt-5">
       <div className="flex w-3xl justify-evenly">
-        <div className="border h-50 w-50 rounded-xl flex justify-center items-center">
-          number stat 1
+        <div className="border h-50 w-50 rounded-xl flex flex-col justify-evenly items-center">
+          <p>Properties:</p>
+          <h1 className="text-7xl">{properties.length}</h1>
         </div>
-        <div className="border h-50 w-50 rounded-xl flex justify-center items-center">
-          number stat 2
+        <div className="border h-50 w-50 rounded-xl flex flex-col justify-evenly items-center">
+          <p>Weekly rental income:</p>
+          <h1 className="text-5xl">
+            $
+            {properties.reduce((acc, property) => acc + property.weeklyRent, 0)}
+          </h1>
         </div>
-        <div className="border h-50 w-50 rounded-xl flex justify-center items-center">
-          number stat 3
+        <div className="border h-50 w-50 rounded-xl flex flex-col justify-evenly items-center">
+          <p>Total Portfolio value:</p>
+          <h1 className="text-4xl">
+            $
+            {properties.reduce((acc, property) => acc + property.purchasePrice, 0)}
+          </h1>
         </div>
       </div>
       <div className="flex w-3xl justify-evenly">
