c74983a62ee4da94a6d6955e66e3089d3be34e69
ronaldkwan93 <ronaldkwan93@gmail.com>
2025-08-27 13:20:07 +1000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center flex-col items-center  ">
+        <h1 className="text-3xl font-bold underline">Hello</h1>
+        <h2>hi</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
