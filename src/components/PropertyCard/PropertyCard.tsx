eb6f90fb0e77ac84e9bd179909563421456b475c
ronaldkwan93 <ronaldkwan93@gmail.com>
2025-08-28 14:59:39 +1000

    adds dashboard view & routing

--- src/components/PropertyCard/PropertyCard.tsx
+++ src/components/PropertyCard/PropertyCard.tsx
@@ -1,5 +1,5 @@
 import type { Property } from "../../context/PropertyContextProvider";
-import logo from "../../assets/logo.png";
+import house from "../../assets/house.jpg";
 import { useNavigate } from "react-router-dom";
 import { FaRegEdit } from "react-icons/fa";
 
@@ -9,27 +9,44 @@ interface PropertyCardProps {
 
 const PropertyCard = ({ property }: PropertyCardProps) => {
   const navigate = useNavigate();
+
+  const roi =
+    property.weeklyRent && property.purchasePrice
+      ? ((property.weeklyRent * 52) / property.purchasePrice) * 100
+      : 0;
   return (
-    <div className="h-40 border rounded-lg shadow p-4 grid grid-cols-[1fr_2fr_1fr]">
+    <div className="h-45 border-none  rounded-lg shadow p-4 grid grid-cols-[1fr_2fr_1fr]">
       <div>
-        <h2 className="text-lg font-bold">Address: {property.address}</h2>
+        <div>
+          <h2 className="text-lg font-bold"> {property.address}</h2>
+        </div>
         <p className="text-gray-600">Suburb: {property.suburb}</p>
+        <p className="text-gray-600">State: {property.state}</p>
         <p className="text-gray-600">
           Purchase price: ${property.purchasePrice}
         </p>
         <p className="text-gray-600">Weekly rent: ${property.weeklyRent}</p>
+        <p
+          className="text-red-600 border w-22 rounded-lg text-center transform transition-transform duration-500 
+               hover:-translate-y-0.5 bg-red-100 cursor-pointer"
+        >
+          ROI: {roi.toFixed(2)}%
+        </p>
       </div>
       <div>
-        <img className="w-20 " src={logo} alt="" />
+        <img
+          className="w-full max-h-30 object-cover rounded-2xl "
+          src={house}
+          alt=""
+        />
       </div>
-      <div className="text-right ">
-        <h1
-          className="cursor-pointer ml-50 w-10 flex justify-center items-center "
-          onClick={() => navigate(`/property/${property.id}`)}
-        >
-          <FaRegEdit />
-        </h1>
-      </div>
+
+      <h1
+        className="cursor-pointer flex justify-center items-center "
+        onClick={() => navigate(`/property/${property.id}`)}
+      >
+        <FaRegEdit />
+      </h1>
     </div>
   );
 };
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,57 +1,96 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { PropertyContext } from "../context/PropertyContextProvider";
+import Charts from "../components/Charts/Charts";
+import { getRecentProperties } from "../services/data-service";
+import RecentPropCard from "../components/PropertyCard/RecentPropCard";
+import { TbHandClick } from "react-icons/tb";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
   const context = useContext(PropertyContext);
+  const [recentProps, setRecentProps] = useState([]);
+  const navigate = useNavigate();
   if (!context)
     throw new Error("PropertyList must be used within PropertyContextProvider");
 
   const { properties } = context;
 
+  useEffect(() => {
+    getRecentProperties().then((result) => setRecentProps(result));
+  }, []);
+
+  const handleViewProperty = () => {
+    navigate("/investments");
+  };
+
+  console.log(recentProps);
+
   return (
-    <div className="m-1 h-screen flex flex-col items-center gap-5 pt-5">
+    <div className="m-1 h-screen bg-neutral-100 flex flex-col items-center gap-5 pt-5 mx-35 rounded-sm my-10">
       <div className="flex w-3xl justify-evenly">
-        <div className="border h-50 w-50 rounded-xl flex flex-col justify-evenly items-center">
+        <div
+          className="border-none h-50 w-50 shadow-lg rounded-xl flex flex-col justify-evenly items-center transform transition-transform duration-200 
+               hover:-translate-y-1 hover:-translate-x-1 bg-blue-100"
+        >
           <p>Properties:</p>
           <h1 className="text-7xl">{properties.length}</h1>
         </div>
-        <div className="border h-50 w-50 rounded-xl flex flex-col justify-evenly items-center">
+        <div
+          className="border-none h-50 w-50 rounded-xl shadow-lg flex flex-col justify-evenly items-center transform transition-transform duration-200 
+               hover:-translate-y-1 hover:-translate-x-1 bg-orange-100"
+        >
           <p>Weekly rental income:</p>
           <h1 className="text-5xl">
             $
-            {properties.reduce((acc, property) => acc + (property.weeklyRent ?? 0), 0)}
+            {properties.reduce(
+              (acc, property) => acc + (property.weeklyRent ?? 0),
+              0
+            )}
           </h1>
         </div>
-        <div className="border h-50 w-50 rounded-xl flex flex-col justify-evenly items-center">
+        <div
+          className="border-none h-50 w-50 rounded-xl shadow-lg flex flex-col justify-evenly items-center transform transition-transform duration-200 
+               hover:-translate-y-1 hover:-translate-x-1 bg-red-100"
+        >
           <p>Total Portfolio value:</p>
           <h1 className="text-4xl">
             $
-            {properties.reduce((acc, property) => acc + (property.purchasePrice ?? 0), 0)}
+            {properties.reduce(
+              (acc, property) => acc + (property.purchasePrice ?? 0),
+              0
+            )}
           </h1>
         </div>
       </div>
       <div className="flex w-3xl justify-evenly">
-        <div className="border h-50 w-50 rounded-xl flex justify-center items-center">
-          number stat 1
+        <Charts data={properties} />
+        {/* <div className="border-none h-50 w-50 rounded-xl flex justify-center items-center">
+          <Charts/>
         </div>
-        <div className="border h-50 w-50 rounded-xl flex justify-center items-center">
+        <div className="border-none h-50 w-50 rounded-xl flex justify-center items-center">
           number stat 2
         </div>
-        <div className="border h-50 w-50 rounded-xl flex justify-center items-center">
+        <div className="border-none h-50 w-50 rounded-xl flex justify-center items-center">
           number stat 3
-        </div>
+        </div> */}
       </div>
-      <div className="flex flex-col w-4/5 justify-evenly items-center gap-2">
-        <h1>Recently added investments..</h1>
-        <div className="border h-15 w-4/5 rounded-xl flex px-5 items-center">
-          number stat 1
+      <div className="flex flex-col w-full justify-evenly items-center gap-2">
+        <div className="flex justify-start w-170 gap-40 ">
+          <button
+            className="flex justify-center items-center cursor-pointer text-2xl transform transition-transform duration-200 
+               hover:-translate-y-1"
+            onClick={handleViewProperty}
+          >
+            <TbHandClick />
+            View
+          </button>
+          <h1>Recently updated investments..</h1>
         </div>
-        <div className="border h-15 w-4/5 rounded-xl flex px-5 items-center">
-          number stat 2
-        </div>
-        <div className="border h-15 w-4/5 rounded-xl flex px-5 items-center">
-          number stat 3
-        </div>
+        {recentProps.map((properties, index) => (
+          <div key={index}>
+            <RecentPropCard property={properties} />
+          </div>
+        ))}
       </div>
     </div>
   );
